b09cb6a01781b9031b355958f65ab3fbd30e72a1
Jbarber <judahbarber@gmail.com>
2023-07-06 15:12:30 -0500

    initial commit/completed lab

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+*@param event {Event}
+*/
+console.log(`hello world`)
+
+const message = document.querySelector(`#message`);
+
+function addMovie(event){
+    event.preventDefault();
+    const imputField = document.querySelector(`input`);
+    const movie = document.createElement(`li`);
+    const movieTitle = document.createElement(`span`);
+    movieTitle.textContent = imputField.value;
+    movieTitle.addEventListener(`click`, crossOffMovie);
+    movie.appendChild(movieTitle);
+    const deleteBtn = document.createElement(`button`);
+    deleteBtn.textContent = `X`;
+    deleteBtn.addEventListener(`click`, deleteMovie);
+    movie.appendChild(deleteBtn);
+    document.querySelector(`ul`).appendChild(movie);
+    imputField.value = ``;
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove();
+    message.textContent = `${event.target.parentNode.querySelector(`span`).textContent} deleted!`;
+    revealMessage();
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle(`checked`);
+    if(event.target.classList.contains(`checked`)){
+        message.textContent = `${event.target.textContent} watched!`;
+    }
+    else{
+        message.textContent = `${event.target.textContent} added back to list!`;
+    }
+    revealMessage();
+}
+
+function revealMessage(){
+    message.classList.remove(`hide`);
+    setTimeout(() => message.classList.add(`hide`), 1000);
+}
+
+document.querySelector(`form`).addEventListener(`submit`, addMovie);
